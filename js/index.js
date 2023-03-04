0c8f4b1cd457fc920c8d9186c003a4e4f6a6c6cc
ShifaaKhalil98 <shifaakhalil50@gmail.com>
2023-03-04 14:56:21 +0200

    added the visibility property to the scroll arrows

--- js/index.js
+++ js/index.js
@@ -1,17 +1,26 @@
 const right_btn = document.getElementById("scroll-right");
 const left_btn = document.getElementById("scroll-left");
+var right_clicks = 0;
+var left_clicks = 0;
+left_btn.style.visibility = "hidden";
 // const leftBtn = document.querySelector(".scroll-left");
 // left_btn.disabled = true;
 right_btn.addEventListener("click", function (event) {
   const content = document.querySelector("#items");
   content.scrollLeft += 300;
-  //   left_btn.disabled = false;
+  left_btn.style.visibility = "visible";
+  right_clicks++;
+  left_clicks--;
+  if (right_clicks > 1) right_btn.style.visibility = "hidden";
   event.preventDefault();
 });
 
 left_btn.addEventListener("click", function (event) {
   const content = document.querySelector("#items");
   content.scrollLeft -= 300;
-  //   left_btn.disabled = false;
+  right_btn.style.visibility = "visible";
+  left_clicks++;
+  right_clicks--;
+  if (left_clicks == 0) left_btn.style.visibility = "hidden";
   event.preventDefault();
 });
