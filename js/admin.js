f5b57a966d4551dd1f49701bbc8c752b36443d4f
ShifaaKhalil98 <shifaakhalil50@gmail.com>
2023-03-06 01:37:54 +0200

    added production insertion validation in js

--- js/admin.js
+++ js/admin.js
@@ -23,3 +23,77 @@ edit_btn.addEventListener("click", function () {
     btn_click = 0;
   }
 });
+
+function add_product() {
+  if (validateForm()) {
+    let product_name = document.getElementById("product-name").value;
+    let for_men = document.getElementById("category").value;
+    let subcategory = document.getElementById("subcategory").value;
+    let brand = document.getElementById("brand").value;
+    let description = document.getElementById("description").value;
+    let product_image = document.getElementById("product-image").value;
+    let product_size = document.getElementById("product-size").value;
+    let product_color = document.getElementById("product-color").value;
+    let product_material = document.getElementById("product-material").value;
+    let price = document.getElementById("price").value;
+    let main_color = document.getElementById("main-color").value;
+
+    let data = new FormData();
+    data.append("product_name", product_name);
+    data.append("price", price);
+    data.append("discount", discount);
+    data.append("for_men", for_men);
+    data.append("subcategory", subcategory);
+    data.append("brand", brand);
+    data.append("description", description);
+    data.append("product_image", product_image);
+    data.append("size", product_size);
+    data.append("main_color", main_color);
+    data.append("product_color", product_color);
+    data.append("product_material", product_material);
+
+    axios({
+      method: "post",
+      url: "http://localhost/shoppero-backend/insert-product.php",
+      data: data,
+    })
+      .then((result) => {
+        console.log(result);
+        if (result.data.status == "added") {
+          alert("Product added successfully");
+          closeForm();
+        } else if (result.data.status == "exists") {
+          alert("Product already exists");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+}
+
+function validateForm() {
+  let product_name = document.getElementById("product-name").value;
+  let description = document.getElementById("description").value;
+  let product_image = document.getElementById("product-image").value;
+  let product_size = document.getElementById("product-size").value;
+  let product_color = document.getElementById("product-color").value;
+  let product_material = document.getElementById("product-material").value;
+  let price = document.getElementById("price").value;
+  let main_color = document.getElementById("main-color").value;
+
+  if (
+    product_name.value.length == 0 ||
+    description.value.length == 0 ||
+    product_image.value.length == 0 ||
+    product_size.value.length == 0 ||
+    product_color.value.length == 0 ||
+    product_material.value.length == 0 ||
+    price.value.length == 0 ||
+    main_color.value.length == 0
+  ) {
+    alert("All fields are required!");
+    return false;
+  }
+  return true;
+}
