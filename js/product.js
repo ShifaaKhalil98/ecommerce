c25870de77927c6c9f8194ac0d401ce25564fc81
MoeShoumar <mks38@mail.aub.edu>
2023-03-06 08:15:33 +0200

    products js main function for item cards

--- js/product.js
+++ js/product.js
@@ -13,5 +13,27 @@ axios.get(`http://localhost/shoppero-backend/get_product.php?id=${id}`).then((re
 let displayProduct = (data)=>{
 
 }
+let displayProducts= (data)=>{
+    for (let index = 0; index < data.length; index++) {
+        const element = data[index];
 
+       products.innerHTML+=`<div class="item-card">
+       <div id="add-to-wishlist"><img src="./assets/heart.png" /></div>
+       <div class="item-img"><img src="./assets/item1.png" /></div>
+       <div>
+           <h3>${element.name}</h3>
+       </div>
+       <div class="flex-box">
+           <h3>${element.price}</h3>
+           <button type="button">Add to cart</button>
+       </div>
+   </div>` 
+   productElem.addEventListener('click', () => {
+    
+    window.location.href = `/product-details.html?id=${element.id}`;
+  });
+
+  products.appendChild(productElem);
+    }
+}
 
