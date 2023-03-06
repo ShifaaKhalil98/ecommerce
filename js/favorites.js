3bd1722e5644c39bc9e41eb282b3795d3133a1fa
Ayman Hajjar <aymanhajjar21@gmail.com>
2023-03-07 01:38:04 +0200

    added multiple functionalities

--- js/favorites.js
+++ js/favorites.js
@@ -1,8 +1,6 @@
+const products = document.getElementById('products')
 
-
-const userID = sessionStorage.getItem("user_id");
-console.log(userID);
-const endpoint = `http://localhost/shoppero-backend/favorites.php?user_id=${userID}`;
+const endpoint = `http://localhost/shoppero_backend/favorites.php`;
 axios.get(endpoint)
   .then(response => {
     const favoriteProducts = response.data;
@@ -19,14 +17,49 @@ let displayProducts= (data)=>{
     products.innerHTML = ''
     data.forEach((element) => {
         console.log(element)
-        products.innerHTML+=`<div class="product">
-        <img src="assets\item1.png" alt="Product 3" />
-        <h2>${element.product_name}</h2>
-        <p class="description">
-        ${element.description}
-        </p>
-        <p class="price">${element.price}</p>
-        <button class="remove-btn">Remove from favorites</button>
+        products.innerHTML+=`<div class="item-card" >
+        <div onclick="addtofavorites(${element.id})" class="item-img"><img id="add-to-wishlist" src="./assets/heart.png" /><img class="product-image" src="./${element.image}" /></div>
+        <div class="product_name"><h3>${element.product_name}</h3></div>
+        <div class="flex-box">
+          <h3>$${element.price}</h3>
+          <button type="button" onclick=(addtoCart(${element.id}))>Add to cart</button>
+        </div>
       </div>`
     })
     }
+
+  function addtofavorites(element) {
+    let heart = document.getElementById('add-to-wishlist');
+    heart.src = 'assets/filled_heart.png'
+    let data = new FormData();
+    data.append('product_id', element);
+
+    axios({
+      "method": "post",
+      "url": "http://localhost/shoppero_backend/add_to_favorites.php",
+      "data": data
+    }).then((result) => {
+        if(result.data.status = 'success') {
+          console.log(result)
+        }
+        
+    }).catch((err) => {
+        console.error(err)
+    });
+  }
+
+  function addtoCart(id) {
+    let data = new FormData();
+    data.append('product_id',id);
+
+    axios({
+      "method": "post",
+      "url": "http://localhost/shoppero_backend/add_to_cart.php",
+      "data": data
+    }).then((result) => {
+      console.log(result)
+        
+    }).catch((err) => {
+        console.error(err)
+    });
+  }
