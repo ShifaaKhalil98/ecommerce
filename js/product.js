bbc49a27dc1a090908a890afdbc4f8e52b2a93a2
MoeShoumar <mks38@mail.aub.edu>
2023-03-06 15:44:39 +0200

    added click event for add to favorites

--- js/product.js
+++ js/product.js
@@ -2,6 +2,7 @@ const description = document.getElementById('description')
 const price = document.getElementById('price')
 const name = document.getElementById('name')
 const products = document.getElementById('products_container')
+const heart = document.getElementById('add-to-wishlist');
 axios({
     "method": "get",
     "url": "http://localhost/shoppero-backend//get_products.php",
@@ -13,3 +14,15 @@ axios({
     console.error(err)
 });
 
+
+heart.addEventListener('click', () => {
+  const user =  sessionStorage.getItem('user_id');
+  const product = 
+  axios.post('http://localhost/shoppero_backend/add_to_favorites.php', { user, product })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
