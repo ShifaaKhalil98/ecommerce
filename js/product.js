e11f7a0c53c381cb7516cb7f37f68bd1e3fa772b
MoeShoumar <mks38@mail.aub.edu>
2023-03-06 08:14:36 +0200

    product js linking

--- js/product.js
+++ js/product.js
@@ -1,2 +1,17 @@
+const description = document.getElementById('description')
+const price = document.getElementById('price')
+const name = document.getElementById('name')
+
+axios.get(`http://localhost/shoppero-backend/get_product.php?id=${id}`).then((res)=>{
+    console.log(res.data);
+}).then((res)=>{
+    displayProduct(res.data)
+}).catch((err)=>{
+    console.error(err)
+});
+
+let displayProduct = (data)=>{
+
+}
 
 
