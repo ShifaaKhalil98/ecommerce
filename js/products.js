a95f63cbfe98baa6002d1dd300122eae3d01c0fb
Ayman Hajjar <aymanhajjar21@gmail.com>
2023-03-06 11:38:01 +0200

    fixed login on products

--- js/products.js
+++ js/products.js
@@ -1,11 +1,27 @@
 const description = document.getElementById('description')
 const price = document.getElementById('price')
 const name = document.getElementById('name')
+const products_container = document.getElementById('products_container')
+const login_status = document.getElementById('status')
 
-axios.get(`http://localhost/shoppero-backend/get_product.php?id=${id}`).then((res)=>{
+axios({
+    "method": "get",
+    "url": "http://localhost/shoppero_backend/check_login.php",
+    withCredentials: true
+  }).then((result) => {
+    console.log(result)
+    if(result.data.success) {
+      login_status.innerHTML = `Welcome, <b>${result.data.first_name}</b>`
+    } else {
+      console.log('noo')
+    }
+  }).catch((err) => {
+    console.error(err)
+  });
+
+axios.get(`http://localhost/shoppero_backend/get_products.php`).then((res)=>{
     console.log(res.data);
-}).then((res)=>{
-    displayProduct(res.data)
+    displayProducts(res.data)
 }).catch((err)=>{
     console.error(err)
 });
@@ -14,28 +30,26 @@ let displayProduct = (data)=>{
 
 }
 let displayProducts= (data)=>{
-    for (let index = 0; index < data.length; index++) {
-        const element = data[index];
-
-       products.innerHTML+=`<div class="item-card">
-       <div id="add-to-wishlist"><img src="./assets/heart.png" /></div>
-       <div class="item-img"><img src="./assets/item1.png" /></div>
-       <div>
-           <h3>${element.name}</h3>
-       </div>
-       <div class="flex-box">
-           <h3>${element.price}</h3>
-           <button type="button">Add to cart</button>
-       </div>
-   </div>` 
-   element.addEventListener('click', () => {
+    data.forEach((element) => {
+        console.log(element)
+        products_container.innerHTML+=`<div class="item-card">
+        <div class="item-img"><img id="add-to-wishlist" src="./assets/heart.png" /><img class="product-image" src="./${element.image}" /></div>
+        <div class="product_name"><h3>${element.product_name}</h3></div>
+        <div class="flex-box">
+          <h3>$${element.price}</h3>
+          <button type="button">Add to cart</button>
+        </div>
+      </div>` 
+//    element.addEventListener('click', () => {
     
-    window.location.href = `/product.html?id=${element.id}`;
-  });
+//     window.location.href = `/product.html?id=${element.id}`;
+//   });
+    })
+        
 
   products.appendChild(element);
     }
-}
+
 
 // save the id of the clicked product to the local storage
 // go to product js and add axious with the id in it
